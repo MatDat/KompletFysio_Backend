e8f9d8e70ff36c14951ad6a0323a30625ec488fc
MatDat <math03t7@stud.kea.dk>
2023-11-20 13:13:35 +0100

    First commit

---
+++ src/main/java/com/example/kompletfysio_backend/KompletFysioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kompletfysio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KompletFysioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KompletFysioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kompletfysio_backend/KompletFysioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kompletfysio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KompletFysioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
