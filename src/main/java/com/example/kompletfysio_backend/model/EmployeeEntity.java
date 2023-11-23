6c9a5a98ba4694f1541b71cc513d93608b389d46
Nademtis <Mathias.99.madsen@gmail.com>
2023-11-23 13:16:42 +0100

    added first_name & last_name, renamed to CPR, changed to LocalDateTime

--- src/main/java/com/example/kompletfysio_backend/model/EmployeeEntity.java
+++ src/main/java/com/example/kompletfysio_backend/model/EmployeeEntity.java
@@ -21,8 +21,11 @@ public class EmployeeEntity {
     @Column(name="employee_id")
     private int employeeId;
 
-    @Column(nullable = false)
-    private String name;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
 
     @Column(nullable = false)
     private String username;
