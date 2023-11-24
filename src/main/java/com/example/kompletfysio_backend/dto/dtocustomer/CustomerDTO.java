68a762a0cf90d0aae5a7d07285359314b6b94294
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-11-24 10:29:43 +0100

    changed name to first + last name, and changed Date to LocalDate for CustomerDTO

--- src/main/java/com/example/kompletfysio_backend/dto/dtocustomer/CustomerDTO.java
+++ src/main/java/com/example/kompletfysio_backend/dto/dtocustomer/CustomerDTO.java
@@ -4,6 +4,6 @@ import com.example.kompletfysio_backend.model.CityEntity;
 
 import java.time.LocalDate;
 
-public record CustomerDTO(int customerId, String name, Date dateOfBirth, String address,
-                          String personalIdentificationNumber, String username, String password, int cityId) {
+public record CustomerDTO(int customerId, String firstName,String lastName, LocalDate dateOfBirth, String address,
+                          String cpr, String username, String password, int cityId) {
 }
