ff49635ea29798eedbc6b4bd0c0791c309822094
Nademtis <Mathias.99.madsen@gmail.com>
2023-12-01 10:48:00 +0100

    TreatmentDTO has durationInMinutes instead of duration_Id

--- src/main/java/com/example/kompletfysio_backend/dto/dtotreamtment/TreatmentDTO.java
+++ src/main/java/com/example/kompletfysio_backend/dto/dtotreamtment/TreatmentDTO.java
@@ -2,5 +2,5 @@ package com.example.kompletfysio_backend.dto.dtotreamtment;
 
 import com.example.kompletfysio_backend.model.DurationEntity;
 
-public record TreatmentDTO(int treatmentId, String treatmentName, int price, int durationId) {
+public record TreatmentDTO(int treatmentId, String treatmentName, int price, int duration) {
 }
