30b78f8ae5b6abf516e0440b8c043d488c25fc57
LauritsNoergaard <laur8709@stud.kea.dk>
2023-12-05 10:16:46 +0100

    Works in postman now

--- src/main/java/com/example/kompletfysio_backend/controller/AppointmentController.java
+++ src/main/java/com/example/kompletfysio_backend/controller/AppointmentController.java
@@ -5,6 +5,8 @@ import com.example.kompletfysio_backend.model.UnavailableEntity;
 import com.example.kompletfysio_backend.service.AppointmentService;
 import com.example.kompletfysio_backend.service.UnavailableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +28,18 @@ public class AppointmentController {
 
     @PostMapping("/appointment")
     public ResponseEntity<AppointmentDTO> createAppointment(@RequestBody AppointmentDTO appointmentDTO){
-        unavailableService.setUnavailableFromAppointment()
-        return appointmentService.save(appointmentDTO);
+        ResponseEntity<AppointmentDTO> saveAppointment = appointmentService.save(appointmentDTO);
+
+        if (saveAppointment.getStatusCode() == HttpStatus.OK) {
+            unavailableService.setUnavailableFromAppointment(
+                    appointmentDTO.startTime(),
+                    appointmentDTO.endtime(),
+                    appointmentDTO.employeeId());
+        } else {
+            System.out.println("Error when saving appointment in AppointmentController /appointment");
+        }
+
+        return saveAppointment;
     }
 
 
