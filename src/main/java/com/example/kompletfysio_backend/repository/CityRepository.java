1e46ddbf1d332f86a2db3755d41f57b6d3337c1b
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-12-08 10:42:28 +0100

    saveCustomer, customer RequestBody

--- src/main/java/com/example/kompletfysio_backend/repository/CityRepository.java
+++ src/main/java/com/example/kompletfysio_backend/repository/CityRepository.java
@@ -4,7 +4,12 @@ import com.example.kompletfysio_backend.model.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Integer> {
     CityEntity findByCityId(int id);
+
+    Optional<CityEntity> findByCityAndZipCode(String city, String zip);
+
 }
